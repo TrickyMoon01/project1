71607714b30e597adc6cb6f3b5397002fc415150
JamesYon <trickymoon01@gmail.com>
2025-07-07 17:14:11 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Spaceman
+
+// define the variables used to track the state of the game
+// for each wrong guess
+    // a part of hangman will be shown
+// for each right guess
+    // a letter will be shown
+// make an updateMessage for if the player has won or lost
+    // IF player chooses a correct letter
+        // THEN letter will show
+    // ELSE IF the player's choice is wrong
+        // THEN show letter in wrong guess board
+// create a reset/play again functionality
+
+// A random word will be picked from an array
